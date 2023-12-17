7d26cc58316de43ad30365b5ed84c2a0974713c6
toum21 <pey2moo1@gmail.com>
2023-12-17 14:05:41 -0800

    created seeds for tickets

--- seeds/ticket-seeds.js
+++ seeds/ticket-seeds.js
@@ -14,9 +14,22 @@ const ticketData = [
         title: 'Machine Took my card',
         description: 'The machine stole my card please help!',
         status: 'open',
-        date_created: 'march 2, 2022',
+        date_created: 'march 15,2022',
         user_id: 3
     },
-    
+    {
+        id: 3,
+        title: 'Machine is awsome',
+        description: 'just wwanted to leave a message saying the machine is awsome for me and the fee is the lowest in town',
+        status: 'open',
+        date_created: 'march 9, 2022',
+        user_id: 1
+    }
 
-]
+];
+
+const seedTicket = () => {
+    return Ticket.bulkCreate(ticketData)
+};
+
+module.exports = seedTicket;
