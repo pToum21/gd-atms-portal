4f410ef2e03bc06e4921048db15f6012405ae446
toum21 <pey2moo1@gmail.com>
2023-12-17 20:58:35 -0800

    updated index.js for seeds

--- seeds/index.js
+++ seeds/index.js
@@ -11,14 +11,11 @@ const seedAll = async () => {
   await seedUser();
   console.log('\n----- USERS SEEDED -----\n');
 
-  await seedCategory();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  await seedTicket();
+  console.log('\n----- TICKETS SEEDED -----\n');
 
-  await seedListing();
-  console.log('\n----- LISTINGS SEEDED -----\n');
-
-  await seedOrder();
-  console.log('\n----- ORDERS SEEDED -----\n');
+  await seedComment();
+  console.log('\n----- COMMENTS SEEDED -----\n');
 
   // process.exit(0) ensures the Node.js process will terminate immediately and return the exit code to the operating system gracefully.
   process.exit(0);
