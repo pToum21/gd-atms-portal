1734272640f249be8094f39b2e11dd6fd4fb6584
pToum21 <pey2moo1@gmail.com>
2023-12-26 14:13:21 -0500

    got handlebars to work properley

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const models = require('./models')
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
