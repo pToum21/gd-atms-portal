77cbac4c497122de5014d8a4ec0d3bdb0ad857a7
pToum21 <pey2moo1@gmail.com>
2023-12-27 18:52:05 -0500

    wprking on the homepage route to show all tickets

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,6 +1,16 @@
-const { User, Ticket, Comment  } = require('../models')
+const { User, Ticket, Comment } = require('../models')
 
 const router = require('express').Router();
 
 const withAuth = require('../utils/auth');
 
+// homepage route
+router.get('/', async (req, res) => {
+try {
+    const ticketData = await Ticket.findAll({
+        
+    })
+} catch (error) {
+    
+}
+})
